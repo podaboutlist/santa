8f3f5b20f64e605b9f0949c1fb61fb287bd3b56b
Ralph Drake <rama.o.ralph@gmail.com>
2020-12-09 14:53:18 -0800

    refactor: move GrinchManager helper class to separate file

--- santabot/cogs/grinch.py
+++ santabot/cogs/grinch.py
@@ -18,43 +18,7 @@
 import discord
 from discord.ext import commands
 from os import getenv
-
-
-class GrinchManager():
-    def __init__(self, webhook: discord.Webhook, name: str, avatar_url: str):
-        """Wrapper class for webhook logic that powers the Grinch's messages.
-
-        Args:
-            webhook (discord.Webhook): The Webhook for sending messages.
-            name (str): The display name of the Webhook
-            avatar_url (str): The URL of the Webhook's avatar.
-        """
-        # TODO: Persist webhook data in the database
-        self.webhook = webhook
-        self.name = name
-        self.avatar_url = avatar_url
-
-    async def send_message(self, message: str):
-        """Sends a message as the Grinch using a webhook.
-
-        Args:
-            message (str): The message to send.
-        """
-        await self.webhook.send(
-            content=message,
-            username=self.name,
-            avatar_url=self.avatar_url
-        )
-
-    async def die(self, killer: str):
-        """Remove the Grinch webhook
-
-        Args:
-            killer (str): The user who killed the Grinch
-        """
-        await self.webhook.delete(
-            reason='{0} banished the Grinch'.format(killer)
-        )
+from ..grinch_manager import GrinchManager
 
 
 class GrinchCommands(commands.Cog):
